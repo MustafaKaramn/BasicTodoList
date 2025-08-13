9628ee3d2568a862b251763b8f0a6d95ea8f5a68
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-08-13 11:16:50 +0300

    Context added. EFCore package added to Business.

---
+++ TodoList.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Entities;
+
+namespace TodoList.DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            modelBuilder.Entity<TodoItem>()
+                .Property(t => t.Description)
+                .HasMaxLength(400);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<TodoItem> TodoLists { get; set; }
+    }
+}
