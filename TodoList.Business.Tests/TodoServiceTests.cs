6a92638a48e50aa4d070018d647b15d3e4a353e7
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-08-17 15:48:55 +0300

    First basic Unit Test structure has been created for GetTodoByIdAsync service.

---
+++ TodoList.Business.Tests/TodoServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using TodoList.Business.DTOs;
+using TodoList.Business.Services;
+using TodoList.Core.Entities;
+using TodoList.DataAccess.Interfaces;
+
+namespace TodoList.Business.Tests
+{
+    public class TodoServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly TodoService _service;
+
+        public TodoServiceTests()
+        {
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _mockMapper = new Mock<IMapper>();
+
+            _service = new TodoService(_mockMapper.Object, _mockUnitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task GetTodoByIdAsync_ShouldReturnTodoDto_WhenTodoExists()
+        {
+            var testId = Guid.NewGuid();
+            var testTodoItem = new TodoItem { Id = testId, Title = "Test Todo", IsCompleted = false, CreatedDate = DateTime.Now };
+            var testTodoDto = new TodoItemDto { Id = testId, Title = "Test Todo", IsCompleted = false, CreatedDate = DateTime.Now };
+
+            _mockUnitOfWork.Setup(uow => uow.TodoRepository.GetByIdAsync(testId)).ReturnsAsync(testTodoItem);
+            _mockMapper.Setup(m => m.Map<TodoItemDto>(testTodoItem)).Returns(testTodoDto);
+
+            //Act
+            var result = await _service.GetTodoByIdAsync(testId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<TodoItemDto>();
+            result.Id.Should().Be(testId);
+            result.Title.Should().Be("Test Todo");
+        }
+
+        [Fact]
+        public async Task GetTodoByIdAsync_ShouldReturnNull_WhenTodoDoesNotExist()
+        {
+            var testId = Guid.NewGuid();
+
+            _mockUnitOfWork.Setup(uow => uow.TodoRepository.GetByIdAsync(testId)).ReturnsAsync((TodoItem)null);
+
+            //Act
+            var result = await _service.GetTodoByIdAsync(testId);
+
+            //Assert
+            result.Should().BeNull();
+        }
+    }
+}
