afe407983125f1a417fb653117d488327e093052
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-08-18 14:42:08 +0300

    TodoList Dtos has been created. TodoList-TodoListDtos mappings  added.

---
+++ TodoList.Business/DTOs/TodoListDTOs/UpdateTodoListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Business.DTOs.TodoListDTOs
+{
+    public class UpdateTodoListDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- TodoList.Business/Mappings/MappingProfile.cs
+++ TodoList.Business/Mappings/MappingProfile.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoList.Business.DTOs.TodoItemDTOs;
+using TodoList.Business.DTOs.TodoListDTOs;
 using TodoList.Core.Entities;
 
 namespace TodoList.Business.Mappings
@@ -13,9 +14,13 @@ namespace TodoList.Business.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<TodoItem, TodoItemDto>();
+            CreateMap<TodoItem, TodoItemDto>().ReverseMap();
             CreateMap<CreateTodoItemDto, TodoItem>();
             CreateMap<UpdateTodoItemDto, TodoItem>();
+
+            CreateMap<TodoList.Core.Entities.TodoList, TodoListDto>().ReverseMap();
+            CreateMap<CreateTodoListDto, TodoList.Core.Entities.TodoList>();
+            CreateMap<UpdateTodoListDto, TodoList.Core.Entities.TodoList>();
         }
     }
 }
