d15e5e136fa16781a58cbfb8ee684628929bcfe7
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-08-30 16:02:35 +0300

    An extension created as named ClaimsPrincipalExtensions to simplify the claim transactions.

---
+++ TodoList.API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace TodoList.API.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetUserId(this ClaimsPrincipal user)
+        {
+            var userIdClaim = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userIdClaim))
+            {
+                throw new UnauthorizedAccessException("User ID claim is missing.");
+            }
+            return Guid.Parse(userIdClaim);
+        }
+    }
+}
