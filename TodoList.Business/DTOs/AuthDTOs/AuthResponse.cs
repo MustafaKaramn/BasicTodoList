eebbf1a58e9f9df1c1adc390ad2d816cfcd5ea2c
Mustafa Karaman <mustafakaraman2552@gmail.com>
2025-08-30 11:56:05 +0300

    Auth DTOs has been created.

---
+++ TodoList.Business/DTOs/AuthDTOs/AuthResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Business.DTOs.AuthDTOs
+{
+    public class AuthResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
